04290459257df3980be115f56f32c13341c2ebe7
sjh1108 <ch6854@naver.com>
2024-12-17 10:27:47 +0900

    feat: Order Services * WrapService

--- src/main/java/store/aurora/order/service/WrapService.java
+++ src/main/java/store/aurora/order/service/WrapService.java
@@ -2,9 +2,12 @@ package store.aurora.order.service;
 
 import store.aurora.order.entity.Wrap;
 
+import java.util.List;
+
 public interface WrapService {
     Wrap createWrap(Wrap wrap);
     Wrap getWrap(Long id);
+    List<Wrap> getWraps();
     void updateWrap(Wrap wrap);
     void deleteWrap(Wrap wrap);
     void deleteByWrapId(Long wrapId);

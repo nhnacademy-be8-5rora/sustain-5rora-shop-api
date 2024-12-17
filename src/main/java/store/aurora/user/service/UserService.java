480c6be7646890b9620278c8c9a27b5327915a9e
ohyumin <oum0607@naver.com>
2024-12-17 16:52:00 +0900

    feat: add oauth 회원등록

--- src/main/java/store/aurora/user/service/UserService.java
+++ src/main/java/store/aurora/user/service/UserService.java
@@ -7,6 +7,7 @@ import store.aurora.user.entity.User;
 
 public interface UserService {
     void registerUser(SignUpRequest signUpRequest);
+    void registerOauthUser(SignUpRequest signUpRequest);
     void deleteUser(String userId);
     void reactivateUser(String userId);
     void checkAndSetSleepStatus();

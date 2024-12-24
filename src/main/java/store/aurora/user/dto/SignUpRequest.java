99fc8b4402cd72a87df3b8595c2bca86bc8442f3
ohyumin <oum0607@naver.com>
2024-12-24 14:27:58 +0900

    feat: singupRequest 전화번호 수정

--- src/main/java/store/aurora/user/dto/SignUpRequest.java
+++ src/main/java/store/aurora/user/dto/SignUpRequest.java
@@ -28,7 +28,7 @@ public class SignUpRequest {
     private String birth;
 
     @NotBlank(message = "전화번호는 필수 항목입니다.")
-    @Pattern(regexp = "^[0-9]{11,}$", message = "전화번호는 숫자만 입력해주세요.")
+    @Pattern(regexp = "^[0-9]{10,11}$", message = "전화번호는 숫자만 입력해주세요.")
     private String phoneNumber;
 
     @NotBlank(message = "이메일은 필수 항목입니다.")

9a39191a4d49e8a2356ed4ddb849898bf4eaf407
ohyumin <oum0607@naver.com>
2024-12-24 14:41:11 +0900

    feat: secure key manager

---
+++ src/main/java/store/aurora/key/KeyConfig.java
@@ -0,0 +1,105 @@
+package store.aurora.key;
+
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.util.List;
+import java.util.Objects;
+
+
+@Configuration
+public class KeyConfig {
+
+    @Value("${key.manager.url}")
+    private String url;
+
+    @Value("${key.manager.app-key}")
+    private String appKey;
+
+    @Value("${key.manager.path}")
+    private String path;
+
+    @Value("${key.manager.password}")
+    private String password;
+
+    public String keyStore(String keyId) {
+        try {
+            // KeyStore 설정
+            KeyStore clientStore = KeyStore.getInstance("PKCS12");
+            InputStream result = new ClassPathResource("5rora.p12").getInputStream();
+            clientStore.load(result, password.toCharArray());
+
+            // SSLContext 설정
+            SSLContext sslContext = SSLContextBuilder.create()
+                    .setProtocol("TLS")
+                    .loadKeyMaterial(clientStore, password.toCharArray())
+                    .loadTrustMaterial(new TrustSelfSignedStrategy())
+                    .build();
+
+            // HttpClient 설정
+            SSLConnectionSocketFactory sslConnectionSocketFactory =
+                    new SSLConnectionSocketFactory(sslContext);
+            PoolingHttpClientConnectionManager connectionManager =
+                    PoolingHttpClientConnectionManagerBuilder.create()
+                            .setSSLSocketFactory(sslConnectionSocketFactory)
+                            .build();
+            CloseableHttpClient httpClient = HttpClients.custom()
+                    .setConnectionManager(connectionManager)
+                    .build();
+
+
+            // RestTemplate 설정
+            HttpComponentsClientHttpRequestFactory requestFactory =
+                    new HttpComponentsClientHttpRequestFactory(httpClient);
+            // HTTP 요청 헤더 설정
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+            RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+            // URI 생성
+            URI uri = UriComponentsBuilder
+                    .fromUriString(url)
+                    .path(path)
+                    .encode()
+                    .build()
+                    .expand(appKey, keyId)
+                    .toUri();
+            // REST API 호출 및 응답 처리
+            return Objects.requireNonNull(restTemplate.exchange(uri,
+                                    HttpMethod.GET,
+                                    new HttpEntity<>(headers),
+                                    KeyResponseDto.class)
+                            .getBody())
+                    .getBody()
+                    .getSecret();
+        } catch (KeyStoreException | IOException | CertificateException
+                 | NoSuchAlgorithmException
+                 | UnrecoverableKeyException
+                 | KeyManagementException e) {
+            throw new KeyManagerException(e.getMessage());
+        }
+    }
+}

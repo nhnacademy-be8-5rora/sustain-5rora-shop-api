7f0fe0efd48a4bd477abc2695deb3c2ee118e67d
00dbgpdnjs <00dbgpdnjs@naver.com>
2024-12-30 16:21:26 +0900

    feat: add user address register api #90

--- src/main/java/store/aurora/user/repository/UserAddressRepository.java
+++ src/main/java/store/aurora/user/repository/UserAddressRepository.java
@@ -1,9 +1,19 @@
 package store.aurora.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import store.aurora.user.entity.UserAddress;
 
+import java.util.Optional;
+
 @Repository
 public interface UserAddressRepository extends JpaRepository<UserAddress, Long> {
+
+    @Query("SELECT ua FROM UserAddress ua WHERE ua.user.id = :userId AND ua.address.id = :addressId AND ua.addrDetail = :addrDetail AND ua.receiver = :receiver")
+    Optional<UserAddress> findDuplicateAddress(@Param("userId") String userId,
+                                               @Param("addressId") Long addressId,
+                                               @Param("addrDetail") String addrDetail,
+                                               @Param("receiver") String receiver);
 }
---
+++ src/main/java/store/aurora/user/service/impl/UserAddressService.java
@@ -0,0 +1,38 @@
+package store.aurora.user.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import store.aurora.common.exception.DataAlreadyExistsException;
+import store.aurora.user.entity.Address;
+import store.aurora.user.entity.User;
+import store.aurora.user.entity.UserAddress;
+import store.aurora.user.exception.UserAddressAlreadyExistsException;
+import store.aurora.user.repository.UserAddressRepository;
+import store.aurora.user.service.UserService;
+
+@Service
+@RequiredArgsConstructor
+public class UserAddressService {
+    private final UserAddressRepository userAddressRepository;
+    private final AddressService addressService;
+    private final UserService userService;
+
+    @Transactional
+    public void addUserAddress(String receiver, String roadAddress, String addrDetail, String userId) {
+        // Address 엔터티 저장 또는 가져오기
+        Address address = addressService.saveOrGetAddress(roadAddress);
+
+        if (userAddressRepository.findDuplicateAddress(
+                userId,
+                address.getId(),
+                addrDetail,
+                receiver
+        ).isPresent()) {
+            throw new UserAddressAlreadyExistsException("중복된 배송지가 이미 존재합니다.");
+        }
+
+        userAddressRepository.save(
+                new UserAddress(addrDetail, address, receiver, userService.getUser(userId)));
+    }
+}

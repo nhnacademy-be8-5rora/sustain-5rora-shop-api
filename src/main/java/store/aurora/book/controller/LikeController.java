de4fb2db80cb19d65314b827c60604ad6e4f7dcf
gimchanghwan <rlackdghks8@naver.com>
2025-01-03 10:44:22 +0900

    좋아요 기능 추가

--- src/main/java/store/aurora/book/controller/LikeController.java
+++ src/main/java/store/aurora/book/controller/LikeController.java
@@ -18,10 +18,11 @@ public class LikeController {
 
     // merge
     @PostMapping("/likes/{bookId}")
-    public ResponseEntity<LikeDto> doLike(@PathVariable Long bookId,
+    public ResponseEntity<Boolean> doLike(@PathVariable Long bookId,
                                           @RequestHeader(value = "X-USER-ID") String userId) {
-        likeService.pressLike(bookId, userId);
-        return ResponseEntity.ok().build();
+        // 서버에서 좋아요를 처리하고, 성공 여부를 boolean 값으로 반환
+        boolean isLiked = likeService.pressLike(bookId, userId);
+        return ResponseEntity.ok(isLiked);  // 좋아요 상태를 true/false로 응답
     }
 
 
--- src/main/java/store/aurora/book/service/LikeService.java
+++ src/main/java/store/aurora/book/service/LikeService.java
@@ -4,7 +4,7 @@ package store.aurora.book.service;
 import store.aurora.book.entity.Like;
 
 public interface LikeService {
-    Like pressLike(Long bookId, String userId);
+    boolean pressLike(Long bookId, String userId);
 
     Like cancelLike(Long bookId, String userId);
 

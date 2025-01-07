abb0cdae941078958c7d1b51b95131e1f5a8da83
sjh1108 <ch6854@naver.com>
2025-01-07 21:37:28 +0900

    fix: OrderRedisConfig * Map<> 형태로 RedisConfig를 작성 했으나,  코드 변경으로 인한 OrderRequestDto 형태로 변경함

--- src/main/java/store/aurora/order/config/OrderRedisConfig.java
+++ src/main/java/store/aurora/order/config/OrderRedisConfig.java
@@ -1,12 +1,12 @@
 package store.aurora.order.config;
 
-import com.fasterxml.jackson.databind.type.TypeFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import store.aurora.order.dto.OrderRequestDto;
 
 import java.util.Map;
 
@@ -14,14 +14,11 @@ import java.util.Map;
 public class OrderRedisConfig {
 
     @Bean(name = "orderRedisTemplate")
-    public RedisTemplate<String, Map<String, Object>> orderRedisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Map<String, Object>> template = new RedisTemplate<>();
+    public RedisTemplate<String, OrderRequestDto> orderRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, OrderRequestDto> template = new RedisTemplate<>();
 
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        Jackson2JsonRedisSerializer<Map<String, Object>> jackson2JsonRedisSerializer
-                = new Jackson2JsonRedisSerializer<>
-                (TypeFactory.defaultInstance()
-                            .constructMapType(Map.class, String.class, Object.class));
+        Jackson2JsonRedisSerializer<OrderRequestDto> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(OrderRequestDto.class);
 
         template.setKeySerializer(stringRedisSerializer);
         template.setValueSerializer(jackson2JsonRedisSerializer);

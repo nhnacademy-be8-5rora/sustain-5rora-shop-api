2c034b6575496fd93b405a6b1a67b9292274c630
sjh1108 <ch6854@naver.com>
2025-01-07 21:19:44 +0900

    feat: Order Controller 에 필요한 메소드 작성 * saveOrderInfoInRedisWithUuid * getOrderResponseFromOrderRequestDtoInRedis
    + 메소드에 관한 설명은 java docs 를 사용해 작성해둠

--- src/main/java/store/aurora/order/controller/OrderController.java
+++ src/main/java/store/aurora/order/controller/OrderController.java
@@ -1,9 +1,9 @@
 package store.aurora.order.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import store.aurora.order.dto.OrderRequestDto;
+import store.aurora.order.dto.OrderResponseDto;
 import store.aurora.order.dto.OrderUuidAndRedirectUrlDto;
 import store.aurora.order.service.process.OrderProcessService;
 
@@ -14,10 +14,25 @@ public class OrderController {
 
     private final OrderProcessService orderProcessService;
 
-    @PostMapping("/save-order-info")
-    public OrderUuidAndRedirectUrlDto saveOrderInfo() {
+    @PostMapping("/create-uuid")
+    public OrderUuidAndRedirectUrlDto createOrderUuidAndResponseUuid(
+            @RequestBody OrderRequestDto orderRequestDto
+            ) {
+        // uuid 생성
         String orderUuid = orderProcessService.getOrderUuid();
+
+        // uuid를 키 값으로 레디스에 저장
+        orderProcessService.saveOrderInfoInRedisWithUuid(orderUuid, orderRequestDto);
+
+        // 생성한 uuid를 redirect url로 변환
         String redirectUrl = "/order/payment?order-id=" + orderUuid;
+
+        //  uuid를 포함한 redirect url 반환
         return new OrderUuidAndRedirectUrlDto(redirectUrl);
+    }
+
+    @GetMapping("/{uuid}/get-order-info")
+    public OrderResponseDto getOrderInfo(@PathVariable String uuid){
+        return orderProcessService.getOrderResponseFromOrderRequestDtoInRedis(uuid);
     }
 }
--- src/main/java/store/aurora/order/service/process/impl/OrderProcessServiceImpl.java
+++ src/main/java/store/aurora/order/service/process/impl/OrderProcessServiceImpl.java
@@ -5,10 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import store.aurora.book.service.BookService;
-import store.aurora.order.dto.OrderDTO;
-import store.aurora.order.dto.OrderDetailDTO;
-import store.aurora.order.dto.OrderedPersonInfoDTO;
-import store.aurora.order.dto.ReceiverInfoDTO;
+import store.aurora.order.dto.*;
 import store.aurora.order.entity.Order;
 import store.aurora.order.entity.OrderDetail;
 import store.aurora.order.entity.Shipment;
@@ -20,7 +17,7 @@ import store.aurora.order.service.process.OrderProcessService;
 import store.aurora.user.entity.User;
 
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 
 @Slf4j
@@ -34,13 +31,9 @@ public class OrderProcessServiceImpl implements OrderProcessService {
     private final ShipmentService shipmentService;
     private final WrapService wrapService;
 
-    private final RedisTemplate<String, Map<String, Object>> orderRedisTemplate;
+    private final RedisTemplate<String, OrderRequestDto> orderRedisTemplate;
 
-    /**
-     * 배송비 계산
-     * 배송비 정책에 의거해 배송비 계산
-     * <pre> 현재: 총 주문 금액이 30,000원 이상일 경우 배송비 무료 </pre>
-     * <pre>
+    /*
      *     todo 배송비 로직 수정
      *      배송비 정책이 수정될 경우를 고려해 setting 테이블에서 배송비 관련 정보를 가져오도록 수정해야 함
      *         이 로직으로는 배송비 정책을 수정할 때마다 코드를 수정하고, 서버를 재배포해야 한다.
@@ -49,9 +42,6 @@ public class OrderProcessServiceImpl implements OrderProcessService {
      *         2단계. 매 주문마다 배송비를 setting에서 불러오는 것이 아닌 캐싱하여 사용
      *             2-1. 특정 시간마다 배송비를 캐싱하고, 배송비를 가져올 때 캐싱된 값을 사용
      *             2-2. 배송비 관련 정보가 변경되었을 때 캐싱된 값을 삭제하고, 새로운 값을 캐싱
-     * </pre>
-     * @param totalAmount 쿠폰을 적용한 총 주문 금액 ( 포인트 적용 전 )
-     * @return 배송비
      */
     @Override
     public int getDeliveryFee(int totalAmount) {
@@ -65,26 +55,28 @@ public class OrderProcessServiceImpl implements OrderProcessService {
         return deliveryFee;
     }
 
-    // todo public이어야 할 필요에 대한 고려 ( public이 아닌 private로 변경해도 되는지 확인 필요 )
     @Override
     public int getTotalAmountFromOrderDetailList(List<OrderDetailDTO> orderDetailList) {
         int totalAmount = 0;
-        for (OrderDetailDTO orderDetail : orderDetailList) {
-            int amount = bookService.getBookById(orderDetail.getBookId()).getSalePrice() * orderDetail.getQuantity();
-            totalAmount += amount - orderDetail.getDiscountAmount();
+        for (OrderDetailDTO detail : orderDetailList) {
+            int amount = bookService.getBookById(detail.getBookId()).getSalePrice()
+                        * detail.getQuantity();
+
+            // wrap 금액 적용
+            if(Objects.nonNull(detail.getWrapId()))
+                amount += wrapService.getWrap(detail.getWrapId()).getAmount()
+                        * detail.getQuantity();
+
+            // 할인 금액 적용
+            if(Objects.nonNull(detail.getDiscountAmount()))
+                amount -= detail.getDiscountAmount();
+
+            totalAmount += amount;
         }
         return totalAmount;
     }
 
     // todo Payment 처리 로직 추가
-    /**
-     * 사용자 주문 처리
-     * @param order OrderDTO 주문 정보
-     * @param orderDetailList List<OrderDetailDTO> 주문 상세 정보
-     * @param receiverInfo ReceiverInfoDTO 수령인 정보
-     * @param user User 사용자 정보
-     * @param orderedPersonInfo OrderedPersonInfoDTO 주문자 정보
-     */
     @Override
     public void userOrderProcess(OrderDTO order,
                                  List<OrderDetailDTO> orderDetailList,
@@ -166,32 +158,55 @@ public class OrderProcessServiceImpl implements OrderProcessService {
         shipmentInformationService.createShipmentInformation(info);
     }
 
-    /**
-     * 주문 번호 uuid 생성
-     * @return uuid 주문 번호
-     */
     @Override
     public String getOrderUuid(){
         return UUID.randomUUID().toString();
     }
 
-    /**
-     * 주문 정보를 uuid를 key로 하여 redis에 저장
-     * @param uuid 주문 번호
-     * @param orderInfo 주문 정보
-     */
     @Override
-    public void orderInfoSaveInRedisWithUuid(String uuid, Map<String, Object> orderInfo){
+    public void saveOrderInfoInRedisWithUuid(String uuid, OrderRequestDto orderInfo){
         orderRedisTemplate.opsForValue().set(uuid, orderInfo);
     }
 
-    /**
-     * uuid를 key로 하여 redis에서 주문 정보를 가져옴
-     * @param uuid 주문 번호 ( key )
-     * @return 주문 정보
-     */
     @Override
-    public Map<String, Object> getOrderInfoFromRedis(String uuid){
-        return orderRedisTemplate.opsForValue().get(uuid);
+    public OrderResponseDto getOrderResponseFromOrderRequestDtoInRedis(String uuid){
+        OrderResponseDto response = new OrderResponseDto();
+
+        OrderRequestDto dto = orderRedisTemplate.opsForValue().get(uuid);
+
+        // customerKey 생성
+        String customerKey = UUID.randomUUID().toString().replace("-", "");
+
+        // Amount 생성
+        String currency = "KRW";
+
+        /*
+            OrderDetailList 를 통해서 다음 내용 ( value, orderName ) 생성함
+
+            todo: OrderDetailDto마다 쿠폰 할인 금액 적용
+             for-each : orderDetailList
+                discountAmount = coupon 할인가
+         */
+        List<OrderDetailDTO> orderDetailList = Objects.requireNonNull(dto).getOrderDetailDTOList();
+
+        int value = getTotalAmountFromOrderDetailList(orderDetailList);
+
+        // OrderName 생성
+        StringBuilder orderName = new StringBuilder();
+        for(OrderDetailDTO detail : orderDetailList){
+            orderName.append(detail.getBookId())
+                    .append(detail.getQuantity())
+                    .append(Objects.nonNull(detail.getWrapId()) ? detail.getWrapId() : "")
+                    .append(Objects.nonNull(detail.getCouponId()) ? detail.getCouponId() : "");
+        }
+
+
+        // response setting
+        response.setCustomerKey(customerKey);
+        response.setCurrency(currency);
+        response.setValue(value);
+        response.setOrderName(orderName.toString());
+
+        return response;
     }
 }

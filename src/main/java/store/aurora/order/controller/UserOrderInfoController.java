47c13a0f569c5b770a343247b59a434dff8bf289
seungchan <tmdcks3489@gmail.com>
2025-01-13 14:19:13 +0900

    🐛 Encryt user id
    userid를 암호화/복호화해서 사용

--- src/main/java/store/aurora/order/controller/UserOrderInfoController.java
+++ src/main/java/store/aurora/order/controller/UserOrderInfoController.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
+import store.aurora.common.encryptor.SimpleEncryptor;
 import store.aurora.order.dto.OrderInfoDto;
 import store.aurora.order.service.UserOrderInfoService;
 
@@ -17,9 +18,10 @@ public class UserOrderInfoController {
     private static final Logger log = LoggerFactory.getLogger("user-logger");
 
     private final UserOrderInfoService userOrderInfoService;
+    private final SimpleEncryptor simpleEncryptor;
 
     @GetMapping
-    public Page<OrderInfoDto> getOrderInfoList(@PathVariable("user-id") String userId, Pageable pageable){
-        return userOrderInfoService.getOrderInfos(userId, pageable);
+    public Page<OrderInfoDto> getOrderInfoList(@PathVariable("user-id") String encryptedId, Pageable pageable){
+        return userOrderInfoService.getOrderInfos(simpleEncryptor.decrypt(encryptedId), pageable);
     }
 }

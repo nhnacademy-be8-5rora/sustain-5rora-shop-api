3d0cd2a8ec05e44aff564d2112a850276d7f413f
sjh1108 <ch6854@naver.com>
2025-01-22 05:16:05 +0900

    test: Test PaymentServiceImpl class

--- src/main/java/store/aurora/order/service/impl/PaymentServiceImpl.java
+++ src/main/java/store/aurora/order/service/impl/PaymentServiceImpl.java
@@ -3,27 +3,37 @@ package store.aurora.order.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import store.aurora.order.entity.Payment;
+import store.aurora.order.repository.OrderRepository;
 import store.aurora.order.repository.PaymentRepository;
 import store.aurora.order.service.PaymentService;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class PaymentServiceImpl implements PaymentService {
 
     private final PaymentRepository paymentRepository;
+    private final OrderRepository orderRepository;
 
     @Override
     public Payment createPayment(Payment payment) {
+        if(Objects.isNull(payment)){
+            throw new IllegalArgumentException("Payment cannot be null");
+        }
+
+        if(Objects.isNull(payment.getStatus())){
+            throw new IllegalArgumentException("Payment status cannot be null");
+        }
+
+        if(Objects.isNull(payment.getOrder())){
+            throw new IllegalArgumentException("Payment order cannot be null");
+        }
+
+        if(Objects.isNull(payment.getOrder().getId()) || !orderRepository.existsById(payment.getOrder().getId())){
+            throw new IllegalArgumentException("Payment order id is invalid");
+        }
+
         return paymentRepository.save(payment);
-    }
-
-    @Override
-    public Payment getPaymentById(Long paymentId) {
-        return paymentRepository.getReferenceById(paymentId);
-    }
-
-    @Override
-    public void deletePaymentById(Long paymentId) {
-        paymentRepository.deleteById(paymentId);
     }
 }
